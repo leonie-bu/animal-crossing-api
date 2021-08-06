b844292af09543f0c01cd3b9f88f0ecd947167b4
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-06 11:31:11 +0200

    ✨ Import createElement function into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './style.css';
+import { createElement } from './utils/createElement';
+
+const mainElement = document.createElement('main');
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  `;
+  app.append(mainElement);
 }
