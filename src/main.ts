7124fa42d940d3fcb23f657bd0584bd6ac5d5829
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-06 12:02:41 +0200

    💄 Create var mainElement including heading&input

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import './style.css';
 import { createElement } from './utils/createElement';
 
-const mainElement = document.createElement('main');
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerText: 'Animal Crossing: New Horizon',
+      className: 'header',
+    }),
+    createElement('input', {
+      placeholder: 'Find your favorite sea creature',
+    }),
+  ],
+});
 
 const app = document.querySelector<HTMLDivElement>('#app');
-
 if (app !== null) {
   app.append(mainElement);
 }
