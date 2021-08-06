32253ae381e2400075571f92c00bce71ae44dfb8
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-06 14:02:02 +0200

    🍱 Basic structure for sea creatures card

---
+++ src/components/seacreatures/seacreatures.ts
@@ -0,0 +1,26 @@
+import { createElement } from '../../utils/createElement';
+
+export function createSeaCreatureCard(): HTMLElement {
+  return createElement('div', {
+    className: 'seaCreature-card',
+    childElements: [
+      createElement('h3', {
+        innerText: 'Seaweed',
+        className: 'seaCreature__name',
+      }),
+      createElement('img', {
+        className: 'seaCreature__image',
+        src: 'https://acnhapi.com/v1/images/sea/1',
+        alt: 'The icon of the sea creature',
+      }),
+      createElement('p', {
+        className: 'seaCreature__price',
+        innerText: 'Pay ' + '600' + 'for me',
+      }),
+      createElement('p', {
+        className: 'seaCreature__catchphrase',
+        innerText: 'I got some seaweed! I could not kelp myself.',
+      }),
+    ],
+  });
+}
