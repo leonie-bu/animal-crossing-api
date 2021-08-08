0a1f95d8253e34ddb82d2b7f95aa260041824622
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-08 18:04:29 +0200

    🎨 Update functions to get and search for sea creatures

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,36 @@
 import { SeaCreatureCharacter, AllSeaCreaturesFromAPI } from '../types';
 
 export async function getSeaCreature(): Promise<SeaCreatureCharacter[]> {
-  const response = await fetch(`https://acnhapi.com/v1/sea/?name=`);
+  const response = await fetch(`https://acnhapi.com/v1/sea/`);
 
+  // Variable mit den Namen data und dem Datentyp von AllSeaCreaturesFromAPI
   const data: AllSeaCreaturesFromAPI = await response.json();
+
+  // Object.values: returns an array of a given object's own property values
+  // --> the values of the object data are turned into an array seacreatures
   const seacreatures = Object.values(data);
+
   const formattedSeaCreatures: SeaCreatureCharacter[] = seacreatures.map(
-    (seacreatures) => {
+    (seacreature) => {
       const formattedSeaCreature: SeaCreatureCharacter = {
-        name: seacreatures.name['name-EUen'],
-        thumbnail: seacreatures.image_uri,
-        price: seacreatures.price,
-        catchphrase: seacreatures['catch-phrase'],
+        name: seacreature.name['name-EUen'],
+        thumbnail: seacreature.image_uri,
+        price: seacreature.price,
+        catchphrase: seacreature['catch-phrase'],
       };
       return formattedSeaCreature;
     }
   );
   return formattedSeaCreatures;
+}
+
+export function searchSeaCreature(
+  allSeaCreatures: SeaCreatureCharacter[],
+  name: string
+): SeaCreatureCharacter[] {
+  const needle = name.toLowerCase();
+  const filteredSeaCreatures = allSeaCreatures.filter(
+    (seaCreatures) => seaCreatures.name.toLowerCase().indexOf(needle) !== -1
+  );
+  return filteredSeaCreatures;
 }
